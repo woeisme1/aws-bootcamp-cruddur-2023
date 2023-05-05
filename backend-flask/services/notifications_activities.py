6ef868ce770c007c564f78119c7421b99343e92d
woeisme1 <glodilebo@gmail.com>
2023-05-05 19:42:09 +0000

    implement frontend notifications page

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -22,5 +22,23 @@ class NotificationsActivities:
         'created_at': (now - timedelta(days=2)).isoformat()
       }],
     },
+    {
+      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+      'handle':  '2Short',
+      'message': 'I wish I was a little bit taller',
+      'created_at': (now - timedelta(days=7)).isoformat(),
+      'expires_at': (now + timedelta(days=9)).isoformat(),
+      'likes': 0,
+      'replies': []
+    },
+    {
+      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+      'handle':  'Garek',
+      'message': 'My dear doctor, I am just simple tailor',
+      'created_at': (now - timedelta(hours=1)).isoformat(),
+      'expires_at': (now + timedelta(hours=12)).isoformat(),
+      'likes': 0,
+      'replies': []
+    }
     ]
     return results
